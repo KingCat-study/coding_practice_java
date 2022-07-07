4a42dfdda62b44dc1c364cad00789c3085381ea6
KingCat-study <jihunrock@gmail.com>
2022-07-07 21:18:11 +0900

    인프런 - 자바(Java) 알고리즘 문제풀이 : 코딩테스트 대비 4번 단어 뒤집기 5번 특정 문자 뒤집기

---
+++ src/inflearn/java_algorithm_solving/PickedCharacterReverse_1_05.java
@@ -0,0 +1,53 @@
+package inflearn.java_algorithm_solving;
+
+/*
+
+설명
+
+영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
+
+특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+
+
+입력
+첫 줄에 길이가 100을 넘지 않는 문자열이 주어집니다.
+
+
+출력
+
+
+입력 값 : kdj#@kdjg%$#kdjgk@kd$dk
+
+정답 값 : kdd#@kkgj%$#dkgjd@kj$dk
+
+ */
+public class PickedCharacterReverse_1_05 {
+    public static void main(String[] args) {
+
+        String str = "kdj#@kdjg%$#kdjgk@kd$dk";
+        String result = "kdd#@kkgj%$#dkgjd@kj$dk";
+        char[] chars = str.toCharArray();
+
+        int firstIndex = 0;
+        int lastIndex = chars.length - 1;
+
+        while(firstIndex < lastIndex) {
+            if(!Character.isAlphabetic(chars[firstIndex])) {
+                firstIndex++;
+            } else if(!Character.isAlphabetic(chars[lastIndex])) {
+                lastIndex--;
+            } else {
+
+                char tmp = chars[firstIndex];
+                chars[firstIndex] = chars[lastIndex];
+                chars[lastIndex] = tmp;
+                firstIndex++;
+                lastIndex--;
+            }
+        }
+
+        String charsToStr = new String(chars);
+
+        System.out.println(charsToStr.equals(result));
+    }
+}
